2e654cb0e69bcc496fdb526386efea0154a8d924
peregr956 <123204430+peregr956@users.noreply.github.com>
2025-07-30 16:13:25 -0400

    Update investment_account.py

--- simulator/investment_account.py
+++ simulator/investment_account.py
@@ -1,15 +1,15 @@
 class InvestmentAccount:
-    def __init__(self, name, balance, annual_return, contribution, employer_match=0, tax_type='tax_deferred'):
+    def __init__(self, name, balance, annual_return, contributions, employer_match=0, tax_type='tax_deferred'):
         self.name = name
         self.balance = balance
         self.annual_return = annual_return
-        self.contribution = contribution
+        self.contributions = contributions
         self.employer_match = employer_match
         self.tax_type = tax_type
         self.history = []
 
-    def project_year(self, is_contributing):
+    def project_year(self, year_index, is_contributing):
         if is_contributing:
-            self.balance += self.contribution + self.employer_match
+            self.balance += self.contributions[year_index] + self.employer_match
         self.balance *= (1 + self.annual_return)
         self.history.append(self.balance)
